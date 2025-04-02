96717e686c79a1f3717c75d72fc37e1897932f67
Ian Sandi <ian.tayler.sandi@gmail.com>
2025-04-02 15:36:40 -0600

    Add project files.

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    cout << "Git is now tracking this project!" << endl;
+
+    int age;
+    cout << "Enter your age" << endl;
+    cin >> age;
+    cout << "You are " << age << " years old" << endl;
+    return 0;
+}
