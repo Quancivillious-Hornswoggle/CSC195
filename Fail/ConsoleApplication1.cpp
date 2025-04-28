740c22a2fdc134fec18732ffd9463ac6fcadba3b
Ian Sandi <ian.tayler.sandi@gmail.com>
2025-04-27 22:50:13 -0600

    aDDED fAIL

---
+++ Fail/ConsoleApplication1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include "Rectangle.h"
+#include "Circle.h"
+
+using namespace std;
+
+int main() {
+    vector<Shape*> shapes;
+
+    Circle* c = new Circle(1.0f);
+    c -> SetRadius(3.0f);
+    Rectangle* r = new Rectangle(4.0f, 5.0f);
+
+    shapes.push_back(c);
+    shapes.push_back(r);
+
+    for (int i = 0; i < shapes.size(); i++)
+    {
+        cout << "Area: " << shapes[i]->Area() << endl;
+        // TODO: If shape is a circle, print radius
+    }
+
+    // FIXME: Getting memory leak! Delete all shapes.
+
+    return 0;
+}
